ca693d34777bff663e7cccc07728d4efa870a432
Khor Chin Heong <koochyrat@gmail.com>
2022-01-07 17:36:21 +0800

    Test get system properties for hand tracking stuff

--- OpenXRNativeWrapper.cs
+++ OpenXRNativeWrapper.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.OpenXR;
 using UnityEngine.XR.OpenXR.Features;
 #if UNITY_EDITOR
 using UnityEditor.XR.OpenXR.Features;
 #endif
 
 #if UNITY_EDITOR
-[OpenXRFeature(UiName = "OpenXR Culling Fix")]
+[OpenXRFeature(UiName = "OpenXR Culling Fix",
+                OpenxrExtensionStrings = "XR_EXT_hand_tracking XR_EXT_hand_joints_motion_range")]
 #endif
 public class OpenXRNativeWrapper : OpenXRFeature
 {
@@ -33,6 +35,62 @@ public class OpenXRNativeWrapper : OpenXRFeature
     internal delegate int _xrLocateViewsFunc(ulong session, in XrViewLocateInfo viewLocateInfo, out XrViewState viewState, int viewCapacityInput, out int viewCountOutput, ref XrView views);
     [MarshalAs(UnmanagedType.FunctionPtr)]
     internal _xrLocateViewsFunc _xrLocateViews;
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    internal delegate int _xrGetSystemFunc(ulong instance, in XrSystemGetInfo getInfo, out ulong systemId);
+    [MarshalAs(UnmanagedType.FunctionPtr)]
+    internal _xrGetSystemFunc _xrGetSystem;
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    internal delegate int _xrGetSystemPropertiesFunc(ulong instance, ulong systemId, ref XrSystemProperties properties);
+    [MarshalAs(UnmanagedType.FunctionPtr)]
+    internal _xrGetSystemPropertiesFunc _xrGetSystemProperties;
+
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    public struct XrSystemGetInfo
+    {
+        public const int TYPE = 4;
+        public int type;
+        public IntPtr next;
+        public int formFactor; //1 for vr
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    public struct XrSystemGraphicsProperties
+    {
+        public int maxSwapchainImageHeight;
+        public int maxSwapchainImageWidth;
+        public int maxLayerCount;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    public struct XrSystemTrackingProperties
+    {
+        public int orientationTracking; //0 false 1 true
+        public int positionTracking;    //0 false 1 true
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    public struct XrSystemProperties
+    {
+        public const int TYPE = 5;
+        public int type;
+        public IntPtr next;
+        public ulong systemId;
+        public int vendorId;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)] public char[] systemName;
+        public XrSystemGraphicsProperties graphicsProperties;
+        public XrSystemTrackingProperties trackingProperties;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    public struct XrSystemHandTrackingPropertiesEXT
+    {
+        public const int TYPE = 1000051000; // XR_TYPE_SYSTEM_HAND_TRACKING_PROPERTIES_EXT = 1000051000
+        public int type;
+        public IntPtr next;
+        public int supportsHandTracking;
+    }
 
     [Serializable]
     [StructLayout(LayoutKind.Sequential, Pack = 0)]
@@ -107,12 +165,17 @@ public class OpenXRNativeWrapper : OpenXRFeature
     private ulong xrInstance;
     private ulong xrSession;
     private ulong xrSpace;
+    private ulong xrSystemId;
     public static bool isInit;    //dont read views until this is true
     //this contains the stereo views, left then right eye
     public static XrView[] views = new XrView[] { new XrView { type = XrView.TYPE }, new XrView { type = XrView.TYPE } };
 
     protected override bool OnInstanceCreate(ulong xrInstance)
     {
+        //foreach (string s in OpenXRRuntime.GetAvailableExtensions())
+        //    Debug.Log(s);
+        foreach (string s in OpenXRRuntime.GetEnabledExtensions())
+            Debug.Log(s);
         this.xrInstance = xrInstance;
         return true;
     }
@@ -135,6 +198,14 @@ public class OpenXRNativeWrapper : OpenXRFeature
         res = _xrGetInstanceProcAddr(xrInstance, "xrLocateViews", out newProcAddr);
         Debug.Assert(res == 0);
         _xrLocateViews = Marshal.GetDelegateForFunctionPointer<_xrLocateViewsFunc>(newProcAddr);
+
+        res = _xrGetInstanceProcAddr(xrInstance, "xrGetSystem", out newProcAddr);
+        Debug.Assert(res == 0);
+        _xrGetSystem = Marshal.GetDelegateForFunctionPointer<_xrGetSystemFunc>(newProcAddr);
+
+        res = _xrGetInstanceProcAddr(xrInstance, "xrGetSystemProperties", out newProcAddr);
+        Debug.Assert(res == 0);
+        _xrGetSystemProperties = Marshal.GetDelegateForFunctionPointer<_xrGetSystemPropertiesFunc>(newProcAddr);
     }
     protected override void OnSessionBegin(ulong xrSession)
     {
@@ -143,6 +214,20 @@ public class OpenXRNativeWrapper : OpenXRFeature
         this.xrSession = xrSession;
 
         int res;
+
+
+        ulong systemId;
+        res = _xrGetSystem(xrInstance, new XrSystemGetInfo { type = XrSystemGetInfo.TYPE, next = IntPtr.Zero, formFactor = 1 }, out systemId);
+        Debug.Assert(res == 0);
+
+        XrSystemHandTrackingPropertiesEXT handy = new XrSystemHandTrackingPropertiesEXT { type = XrSystemHandTrackingPropertiesEXT.TYPE, next = IntPtr.Zero };
+        IntPtr pnt = Marshal.AllocHGlobal(Marshal.SizeOf(handy));
+        Marshal.StructureToPtr(handy, pnt, false);
+        XrSystemProperties sysProp = new XrSystemProperties { type = XrSystemProperties.TYPE, next = pnt };
+        res = _xrGetSystemProperties(xrInstance, systemId, ref sysProp);
+        Debug.Assert(res == 0);
+        handy = Marshal.PtrToStructure<XrSystemHandTrackingPropertiesEXT>(pnt);
+        Marshal.FreeHGlobal(pnt);
 
         XrPosef xrPose = new XrPosef { orientation = Quaternion.identity, position = Vector3.zero };
         //create a reference space of type view so that we can get the left and right eye transforms relative to head
